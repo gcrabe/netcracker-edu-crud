99487a5047a764f84797753020b65213416a4c7a
yayavin <yayavin@yandex.ru>
2017-04-08 17:14:50 +0300

    Signed-off-by: yayavin <yayavin@yandex.ru>

---
+++ CrudLib/src/main/java/com/netcracker/education/crudlib/database/DatabaseRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.netcracker.education.crudlib.database;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ya
+ */
+public interface DatabaseRepository {
+    public void create(String dbName);
+    public void delete(String dbName);
+    public void update(String dbName, String newDbName);
+    public Database getByName(String dbName);
+    public List<String> getAllNames();
+}
---
+++ CrudLib/src/main/java/com/netcracker/education/crudlib/database/impl/DatabaseRepositoryImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.netcracker.education.crudlib.database.impl;
+
+import com.netcracker.education.crudlib.database.Database;
+import com.netcracker.education.crudlib.database.DatabaseRepository;
+import java.util.List;
+
+/**
+ *
+ * @author Ya
+ */
+public class DatabaseRepositoryImpl implements DatabaseRepository{
+
+    @Override
+    public void create(String dbName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String dbName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(String dbName, String newDbName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Database getByName(String dbName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getAllNames() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
