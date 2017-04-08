b85d800f26345f47094f812ebce70e2ccc56d6db
yayavin <yayavin@yandex.ru>
2017-04-08 18:09:51 +0300

    Signed-off-by: yayavin <yayavin@yandex.ru>

--- CrudLib/src/main/java/com/netcracker/education/crudlib/database/Database.java
+++ CrudLib/src/main/java/com/netcracker/education/crudlib/database/Database.java
@@ -5,6 +5,7 @@
  */
 package com.netcracker.education.crudlib.database;
 
+import com.netcracker.education.crudlib.table.Table;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,5 +16,5 @@ import java.util.Map;
 public class Database {
     String name;
     String path;
-    //Map<String, Table> tables = new HashMap<>(); // tableName, table
+    Map<String, Table> tables = new HashMap<>(); // tableName, table
 }
