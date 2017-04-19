7ca98e97358ad6d70d08b7430b5a418bc1d87066
yayavin <yayavin@yandex.ru>
2017-04-19 12:38:03 +0300

    Signed-off-by: yayavin <yayavin@yandex.ru>

--- CrudLib/src/main/java/com/netcracker/education/crudlib/database/impl/DatabaseRepositoryImpl.java
+++ CrudLib/src/main/java/com/netcracker/education/crudlib/database/impl/DatabaseRepositoryImpl.java
@@ -115,15 +115,8 @@ public class DatabaseRepositoryImpl implements DatabaseRepository{
     public boolean update(Database database, Database newDatabase){
         
         boolean updateTemp = update(database.getName(), newDatabase.getName());
-        if(!updateTemp){
-            return false;
-        }
         
-        StringBuilder msg = new StringBuilder();
-        msg.append("Database [").append(database.getName()).append("] successfully renamed to [").append(newDatabase.getName()).append("].");
-        LOGGER.info(msg.toString(), Level.INFO);
-        
-        return true;
+        return updateTemp;
     }
 
     @Override
